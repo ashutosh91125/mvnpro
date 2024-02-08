6beeef52ee218c042ec74da4a5e00a08a39daf2e
Ashutosh Mishra <ashutosh.garg527@gmail.com>
2024-02-08 06:43:16 +0530

    first push

---
+++ mysearch/src/main/java/mysearch/SearchController.java
@@ -0,0 +1,29 @@
+package mysearch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+
+	@RequestMapping("/home")
+	public String home() {
+		
+		System.out.println("Home page");
+		
+		return "home";
+	}
+	
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("queryBox") String query) {
+		
+		RedirectView redirectView = new RedirectView();
+		String url = "https://www.google.com/search?q=" + query;
+		redirectView.setUrl(url);
+		return redirectView;
+		
+	}
+	
+}
